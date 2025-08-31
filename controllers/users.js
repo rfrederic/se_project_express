5572aa0a28259f43906ee6ca60d6682a8617f046
RigaudsonFrederic <frigaudson1997@gmail.com>
2025-08-31 16:59:51 -0400

    new update

--- controllers/users.js
+++ controllers/users.js
@@ -1,28 +1,56 @@
 const User = require("../models/User");
 const STATUS = require("../utils/statusCodes");
 
+// GET all users
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.status(STATUS.OK).send(users))
-    .catch((err) => {
-      console.error(err);
-      return res
+    .catch(() => {
+      res
         .status(STATUS.INTERNAL_SERVER_ERROR)
         .send({ message: "An error has occurred on the server" });
     });
 };
 
+// GET user by ID
+const getUserById = (req, res) => {
+  const { userId } = req.params;
+
+  User.findById(userId)
+    .then((user) => {
+      if (!user) {
+        return res.status(STATUS.NOT_FOUND).send({ message: "User not found" });
+      }
+      res.status(STATUS.OK).send(user);
+    })
+    .catch((err) => {
+      if (err.name === "CastError") {
+        return res
+          .status(STATUS.BAD_REQUEST)
+          .send({ message: "Invalid user ID" });
+      }
+      res
+        .status(STATUS.INTERNAL_SERVER_ERROR)
+        .send({ message: "An error has occurred on the server" });
+    });
+};
+
+// CREATE user
 const createUser = (req, res) => {
   const { name, avatar } = req.body;
 
   User.create({ name, avatar })
-    .then((user) => res.status(STATUS.CREATED).send({ data: user }))
+    .then((user) => res.status(STATUS.CREATED).send(user))
     .catch((err) => {
-      console.error(err);
-      res.status(STATUS.INTERNAL_SERVER_ERROR).send({
-        message: "An error has occurred on the server",
-      });
+      if (err.name === "ValidationError") {
+        return res
+          .status(STATUS.BAD_REQUEST)
+          .send({ message: "Invalid user data" });
+      }
+      res
+        .status(STATUS.INTERNAL_SERVER_ERROR)
+        .send({ message: "An error has occurred on the server" });
     });
 };
 
-module.exports = { getUsers, createUser };
+module.exports = { getUsers, getUserById, createUser };
