a2d78d6679ff7d74cb223b2d5c88ec7dc9f43c73
RigaudsonFrederic <frigaudson1997@gmail.com>
2025-10-23 13:16:09 -0400

    new update

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,7 +2,7 @@ const STATUS = require("../utils/statusCodes");
 
 module.exports = (err, req, res, next) => {
   console.error(err.stack || err.message);
-  const statusCodes = err.statusCodes || INTERNAL_SERVER_ERROR;
+  const statusCodes = err.statusCodes || STATUS.INTERNAL_SERVER_ERROR;
   const message = err.message || "An unexpected error occurred on the server.";
   res.status(statusCodes).send({ message });
 };
